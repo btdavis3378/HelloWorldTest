6c7dc3cc442b74d7330dc0d94f196ad46d87abd5
Brandon <btdavis3378@eagle.fgcu.edu>
2018-09-08 20:48:37 -0400

    Testing Java FX tutorial 2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,24 +1,70 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 //Comment test
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) {
+    GridPane root = new GridPane();
+
+    root.setPadding(new Insets(20));
+    root.setHgap(25);
+    root.setVgap(15);
+
+    Label labelTitle = new Label("Welcome");
+
+    // Put on cell (0,0), span 2 column, 1 row.
+    root.add(labelTitle, 0, 0, 2, 1);
+
+    Label labelUserName = new Label("User Name");
+    TextField fieldUserName = new TextField();
+
+    Label labelPassword = new Label("Password");
+
+    PasswordField fieldPassword = new PasswordField();
+
+    Button loginButton = new Button("Login");
+
+    GridPane.setHalignment(labelUserName, HPos.RIGHT);
+
+    // Put on cell (0,1)
+    root.add(labelUserName, 0, 1);
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    GridPane.setHalignment(labelPassword, HPos.RIGHT);
+    root.add(labelPassword, 0, 2);
+
+    // Horizontal alignment for User Name field.
+    GridPane.setHalignment(fieldUserName, HPos.LEFT);
+    root.add(fieldUserName, 1, 1);
+
+    // Horizontal alignment for Password field.
+    GridPane.setHalignment(fieldPassword, HPos.LEFT);
+    root.add(fieldPassword, 1, 2);
+
+    // Horizontal alignment for Login button.
+    GridPane.setHalignment(loginButton, HPos.RIGHT);
+    root.add(loginButton, 1, 3);
+
+    Scene scene = new Scene(root, 300, 300);
+    primaryStage.setTitle("JavaFX Welcome");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
 }
